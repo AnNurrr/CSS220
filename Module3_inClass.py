35d88c1d7c100c4bb409fc77c744a467a7195f7a
AnNurrr <ainuremilbekova9@gmail.com>
2024-01-30 10:00:39 -0600

    Initial commit

---
+++ Module3_inClass.py
@@ -0,0 +1,9 @@
+# Ainur Emilbekova
+# 1/30/24
+
+# problem 1 - Write a Python program to create a set.
+# Create an empty set x and a non-empty set n=(0, 1, 2, 3, 4).
+
+# Create an empty set
+x = set()
+
