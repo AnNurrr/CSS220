0e51ea5bb72561235aa47774e5bade2a877e4b17
AnNurrr <ainuremilbekova9@gmail.com>
2024-02-15 11:40:23 -0600

    changes

---
+++ binarySearch.py
@@ -0,0 +1,21 @@
+# Ainur
+# 2/20/2024
+# CSS 220 Module 6
+
+# Binary Search
+
+numberList=[1, 4, 6, 9, 20, 40, 60]
+number = 20
+l =0
+nlength = len(numberList)
+
+def bSearch(numberList, number, nlength, 1):
+while l<=nlength:
+    mid = l+(nlength -l) //2;
+
+    if numberList[mid] == number:
+        return mid
+    elif numberList[mid] < number:
+        l = mid+1
+    else:
+        nlength = mid-1
