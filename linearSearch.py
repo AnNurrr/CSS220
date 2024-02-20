193692ec9863e2ae5f3550fa799fb68ab3d700b7
AnNurrr <ainuremilbekova9@gmail.com>
2024-02-20 11:18:17 -0600

    changes

---
+++ linearSearch.py
@@ -0,0 +1,13 @@
+# Ainur
+# 2/20/2024
+# CSS 220 Module 6
+
+# Linear Search Alporithm
+
+numberList = [5, 4, 8, 7, 2, 56, 12]
+number = 8
+
+for i in range(0, len(numberList)):
+    if number == numberList:
+        print()
+
